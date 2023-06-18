0647a2a327b9f3fb400a5059d5cf791954bd94a0
tkkhm <takhminashh@gmail.com>
2023-06-18 22:15:54 +0600

    update some func

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,7 +1,21 @@
-const faqs = document.querySelectorAll(".faq")
+function search(){
+    let filter = document.getElementById('find').value.toUpperCase();
 
-faqs.forEach(faq => {
-    faq.addEventListener("click", () => {
-        faq.classList.toggle("active");
-    });
-});
+    let item = document.querySelectorAll('.product');
+
+    let l = document.getElementsByTagName('h3');
+
+    for(var i = 0;i<=l.length;i++){
+        let a=item[i].getElementsByTagName('h3')[0];
+
+        let value=a.innerHTML || a.innerText || a.textContent;
+
+        if(value.toUpperCase().indexOf(filter) > -1){
+            item[i].style.display="";
+        }
+        else
+        {
+            item[i].style.display="none";
+        }
+    }
+}
